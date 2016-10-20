c83ee4ececce916bf1b3f281609434644f3f6c6b
Lee Mordell <lee.mordell@gmail.com>
2016-10-20 09:25:28 -0700

    [setup]basic react environment setup complete

---
+++ Server/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+//App level middleware
+app.use(bodyParser())
+app.use(morgan('dev'))
+app.use(express.static('Public'))
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log(`server is listening on port ${port}`))
+
+
