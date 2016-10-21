63efacd7e898c3cd8a809380747428f7de4fdb50
Lee Mordell <lee.mordell@gmail.com>
2016-10-20 17:46:35 -0700

    [pull]autocomplete is restricted to San Francisco

--- Public/components/map_view.jsx
+++ Public/components/map_view.jsx
@@ -1,42 +1,28 @@
 import React, { Component } from 'react'
 import { GoogleMap, GoogleMapLoader, Marker } from 'react-google-maps'
 
-import {default as FaSpinner} from "react-icons/lib/fa/spinner";
-import {default as ScriptjsLoader} from "react-google-maps/lib/async/ScriptjsLoader";
+import { default as FaSpinner } from 'react-icons/lib/fa/spinner'
+import { default as ScriptjsLoader } from 'react-google-maps/lib/async/ScriptjsLoader'
 
 export default class Map extends Component {
-	constructor(props) {
-		super(props)
-	}
+  constructor (props) {
+    super(props)
+  }
 
-	render() {
-		console.log('this props', this.props)
-		return (
+  render () {
+    return (
 
-			<ScriptjsLoader
-  				  hostname={"maps.googleapis.com"}
-  				  pathname={"/maps/api/js"}
-  				  query={{ key: 'AIzaSyBi5gLmdXNTcFlB04hUijdJ_3fdEUb8bkA' , libraries: "geometry,drawing,places"}}
-				  loadingElement={
-				    <div>
-				      <FaSpinner />
-				    </div>
-				  }
-				  containerElement={
-				    <div className='map' />
-				  }
-				  googleMapElement={
-				    <GoogleMap
-				      defaultZoom={12}
-				      defaultCenter={
-					           	{lat: this.props.lat,
-					           	 lng: this.props.lon
-								}}
-				    >
-				      <Marker {...this.props.markerProps} />
-				    </GoogleMap>
-				  }
-				/>
-		)
-	}
+      <ScriptjsLoader
+        hostname={"maps.googleapis.com"}
+        pathname={"/maps/api/js"}
+        query={{ key: 'AIzaSyBi5gLmdXNTcFlB04hUijdJ_3fdEUb8bkA',  libraries: 'geometry,drawing,places'}}
+        loadingElement={<div>
+                          <FaSpinner />
+                        </div>}
+        containerElement={<div className='map' />}
+        googleMapElement={<GoogleMap defaultZoom={13} defaultCenter={{lat: this.props.lat, lng: this.props.lon }}>
+                            <Marker {...this.props.markerProps} />
+                          </GoogleMap>} />
+    )
+  }
 }
--- Public/components/navbar.jsx
+++ Public/components/navbar.jsx
@@ -1,30 +1,55 @@
 import React, { Component } from 'react'
-import { AppBar, Drawer, MenuItem } from 'material-ui/'
+import Geosuggest from 'react-geosuggest'
+import { AppBar, Drawer, MenuItem, TextField } from 'material-ui/'
 
 export default class NavBar extends Component {
   constructor (props) {
     super(props)
 
-    this.state = { open: false }
+    this.state = { open: false, search: '' }
 
     this.handleOpenSideBar = this.handleOpenSideBar.bind(this)
+    this.handleSearch = this.handleSearch.bind(this)
+  }
+
+  componentDidMount () {
+    let input = document.getElementById('searchTextField')
+    let autocomplete
+    let southWestBounds
+    let northEastBounds
+    let autoCompleteRestrictions
+    // Async - wait for google api to load before defining vals
+    setTimeout(() => {
+      southWestBounds = new google.maps.LatLng(37.689178, -122.501335)
+      northEastBounds = new google.maps.LatLng(37.833844, -122.414818)
+      autoCompleteRestrictions = new google.maps.LatLngBounds(southWestBounds, northEastBounds)
+      let options = { bounds: autoCompleteRestrictions }
+      autocomplete = new google.maps.places.Autocomplete(input, options)
+    }, 200)
   }
 
   handleOpenSideBar () {
     this.setState({ open: !this.state.open })
   }
 
+  handleSearch (e) {
+    this.setState({ search: e.target.value })
+  }
+
   render () {
-    const forceNavDown = { 'top': '72px' }
+    const forceNavDown = { 'top': '64px' }
 
     return (
       <div className='nav'>
         <AppBar onLeftIconButtonTouchTap={this.handleOpenSideBar} title='SF Film Locations' iconClassNameRight='muidocs-icon-navigation-expand-more' />
-        <Drawer containerStyle={forceNavDown} open={this.state.open}>
+        <Drawer containerStyle={forceNavDown} width={272} open={this.state.open}>
           <MenuItem>
-          <input placeholder='Search locations' />
-          </MenuItem>
-          <MenuItem> Details
+          <TextField
+            id='searchTextField'
+            placeholder=''
+            floatingLabelText='Look for Film Locations'
+            fullWidth={true}
+            onChange={this.handleSearch} />
           </MenuItem>
         </Drawer>
       </div>
