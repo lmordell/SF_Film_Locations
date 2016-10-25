73e342671c65a9d3e9606b275961512e8fe48772
Lee Mordell <lee.mordell@gmail.com>
2016-10-25 11:58:40 -0700

    [pull]back-end gets films by titles and adds lat&lng to each location

--- Server/config.js
+++ Server/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-	token: 'YY1DPC0Yb3fwXPLdLl1RHlH6U'
+	token: 'YY1DPC0Yb3fwXPLdLl1RHlH6U',
+	google_api_token: 'AIzaSyBi5gLmdXNTcFlB04hUijdJ_3fdEUb8bkA'
 }
--- Server/movies/movies_model.js
+++ Server/movies/movies_model.js
@@ -38,9 +38,36 @@ const getMovieData = (req, res) => {
   }
 
   rp(options)
-  .then(movies => res.status(200).send(movies))
-  .catch(err => res.status(404).send(err))
- 
+    .then(movies => {
+      // declare as const to access movies array in subsequent .then statement
+      const films = movies
+      // Format returned movie locations to query google geocode api to get lat/lng
+      const queries = movies.map(movie => `${movie.locations.split(' ').join('+')}+San+Francisco`)
+
+      return Promise.all(queries.map((address) => {
+
+        const geoCodeOptions = { method: 'GET',
+          url: 'https://maps.googleapis.com/maps/api/geocode/json',
+          qs: { address: address },
+          headers: { 'cache-control': 'no-cache',
+            'key': config.google_api_token,
+          'content-type': 'application/json' },
+          json: true
+        }
+
+        return rp(geoCodeOptions)
+      }))
+        .then(locations => {
+          // Add lat / lng to each film
+          films.forEach((film, i) => {
+            film.lat = locations[i].results[0].geometry.location.lat
+            film.lng = locations[i].results[0].geometry.location.lng
+          })
+
+          res.status(200).send(films)
+        })
+    })
+    .catch(err => res.status(404).send(err))
 }
 
 module.exports = {
